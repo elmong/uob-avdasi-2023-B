48be8bc797d0b25de3ddb2f42dd3725781b5c2ba
Henrick Ku <36804898+hkkhkhkhk@users.noreply.github.com>
2023-11-20 21:28:13 +0000

    Refresh rate init

--- mavlink_data extract.py
+++ mavlink_data extract.py
@@ -2,15 +2,31 @@
 
 from pymavlink import mavutil
 import numpy as np
+import time
 
-roll = 0
-
-port='com5' # simulator 'tcp:127.0.0.1:5762' , cube com#
+port='tcp:127.0.0.1:5762' # simulator 'tcp:127.0.0.1:5762' , cube com#
 
 connection = mavutil.mavlink_connection(port) #connect to local simulator, change to com'number' 
 
 connection.wait_heartbeat() #heartbeat so make sure it's connected to the flight controller
 
+print("Heartbeat from system (system %u component %u)" % (connection.target_system, connection.target_component))
+
+mav_commands = [mavutil.mavlink.MAVLINK_MSG_ID_ATTITUDE, 
+                mavutil.mavlink.MAVLINK_MSG_ID_AOA_SSA]
+
+# Refresh rate request
+for i in range(len(mav_commands)):
+    message = connection.mav.command_long_send(
+            connection.target_system,  # Target system ID
+            connection.target_component,  # Target component ID
+            mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL,  # ID of command to send
+            0,  # Confirmation
+            mav_commands[i],  # param1: Message ID to be streamed
+            0.2*10**6, # param2: Interval in microseconds
+            0, 0, 0, 0, 
+            0  # target address
+            )
 
 while True:
     msg=connection.recv_match()
@@ -19,8 +35,7 @@ while True:
         continue
     if msg.get_type() == 'ATTITUDE': #get type of message , check mavlink inspector on missionplanner to get type.
         attitude=msg.to_dict() #extract message to dictionary
-        print(attitude)
-        
+        #print(attitude)
         
         yaw=attitude['yaw'] #extract value from dictionary : so 'roll', 'pitch', 'yawspeed'
         print('yaw :' + str(yaw*180/np.pi) +' deg')
@@ -28,4 +43,10 @@ while True:
         print('pitch :' + str(pitch*180/np.pi) +' deg')
         roll=attitude['roll']
         print('roll :' + str(roll*180/np.pi) +' deg')
+
+    if msg.get_type() == 'AOA_SSA': #get type of message , check mavlink inspector on missionplanner to get type.
+        aoa_ssa=msg.to_dict()
+        print('AOA :' + str(aoa_ssa['AOA']))
+
+    time.sleep(0.001)
 
