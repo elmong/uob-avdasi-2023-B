b1f32708a4557fe77aaa6f6bb5b2104ff8a8d35f
IMS14 <147995206+IMS14@users.noreply.github.com>
2024-03-02 12:47:32 +0000

    Update control_surface_map_class.py

--- project directory/control_surface_map_class.py
+++ project directory/control_surface_map_class.py
@@ -39,64 +39,68 @@ class Control_Surface:
         
         try: 
             if self.table[1].index(adc_now) != None: #assuming individual adc values do not exactly repeat
-            #if somehow adc lands on a stored value, return angle directly by index
+                #if somehow adc lands on a stored value, add to possible rates
                 rate = self.table[0][self.table[1].index(adc_now)]
-            return self.table[2][self.table[0].index(rate)]
-        except:       
-            #find all adc values' indexes in table close to adc_now
-            #go through adc values in list
-            index1 = 0
-            for adc_item in self.table[1]:
-                if (adc_now > adc_item) and (self.table[1][index1 + 1] > adc_now):
-                    adc_indexes[0].append(index1 + 1)
-                    adc_indexes[1].append(index1)
-                elif (adc_now < adc_item) and (self.table[1][index1 + 1] <adc_now):
-                    adc_indexes[1].append(index1 + 1)
-                    adc_indexes[0].append(index1)
+                possible_rates.append(rate)
+        except: 
+            #do nothing      
+            pass
+        #continue with rest of code block
+        
+        #find all adc values' indexes in table close to adc_now
+        #go through adc values in list
+        index1 = 0
+        for adc_item in self.table[1]:
+            if (adc_now > adc_item) and (self.table[1][index1 + 1] > adc_now):
+                adc_indexes[0].append(index1 + 1)
+                adc_indexes[1].append(index1)
+            elif (adc_now < adc_item) and (self.table[1][index1 + 1] <adc_now):
+                adc_indexes[1].append(index1 + 1)
+                adc_indexes[0].append(index1)
 
-                if index1 == (len(self.table[1]) -2):
-                    break
-                else:
-                    index1 +=1
-            
-            i = 0 #keeping track of the index of the next for loop
-            if len(adc_indexes[0]) > 0:
-                for index in adc_indexes[0]:
-                    #set up values for linear interpolation, to find potential rates
-                    #x is adc
-                    #y is rate
-                    x = adc_now
-                    x2 = self.table[1][index]
-                    x1 = self.table[1][adc_indexes[1][i]] 
-                    y2 = self.table[0][index]  
-                    y1 = self.table[0][adc_indexes[1][i]]   
-                    #calculate and store possible rates
-                    possible_rates.append(self.linear_interpolation(x,x1,x2,y1,y2))
-                    
-                    i +=1
-                #find closest rate
-                interpolated_rate = self.find_closest_value(possible_rates,rate_now)
-                enclosing_high_rate = None
-                enclosing_low_rate = None
-                angle1 = None
-                angle2 = None
-                #find rate values which are closest to, and enclose, rate now
-                
-                for rate in self.table[0]:
-                    if rate > interpolated_rate:
-                        enclosing_low_rate = self.table[0][self.table[0].index(rate) - 1] #get value just before
-                        enclosing_high_rate = rate
-                        #find corresponding angles for these rates
-                        angle1 = self.table[2][self.table[0].index(enclosing_low_rate)]
-                        angle2 = self.table[2][self.table[0].index(enclosing_high_rate)]
-                        break
-                
-                #interpolate angle from converted rate
-                interpolated_angle = self.linear_interpolation(interpolated_rate,enclosing_low_rate,enclosing_high_rate,angle1,angle2)
-                return interpolated_angle
-            
+            if index1 == (len(self.table[1]) -2):
+                break
             else:
-                return 0
+                index1 +=1
+        
+        i = 0 #keeping track of the index of the next for loop
+        if len(adc_indexes[0]) > 0:
+            for index in adc_indexes[0]:
+                #set up values for linear interpolation, to find potential rates
+                #x is adc
+                #y is rate
+                x = adc_now
+                x2 = self.table[1][index]
+                x1 = self.table[1][adc_indexes[1][i]] 
+                y2 = self.table[0][index]  
+                y1 = self.table[0][adc_indexes[1][i]]   
+                #calculate and store possible rates
+                possible_rates.append(self.linear_interpolation(x,x1,x2,y1,y2))
+                
+                i +=1
+            #find closest rate
+            interpolated_rate = self.find_closest_value(possible_rates,rate_now)
+            enclosing_high_rate = None
+            enclosing_low_rate = None
+            angle1 = None
+            angle2 = None
+            #find rate values which are closest to, and enclose, rate now
+            
+            for rate in self.table[0]:
+                if rate > interpolated_rate:
+                    enclosing_low_rate = self.table[0][self.table[0].index(rate) - 1] #get value just before
+                    enclosing_high_rate = rate
+                    #find corresponding angles for these rates
+                    angle1 = self.table[2][self.table[0].index(enclosing_low_rate)]
+                    angle2 = self.table[2][self.table[0].index(enclosing_high_rate)]
+                    break
+            
+            #interpolate angle from converted rate
+            interpolated_angle = self.linear_interpolation(interpolated_rate,enclosing_low_rate,enclosing_high_rate,angle1,angle2)
+            return interpolated_angle
+        
+        else:
+            return 0
         
 
     #for a given rate value find angle
