d5bb669995831921db8754d1ee9ca666f2da1998
IMS14 <147995206+IMS14@users.noreply.github.com>
2024-03-03 22:49:13 +0000

    created manual plotting script

--- project directory/manual_plotter.py
+++ project directory/manual_plotter.py
@@ -1,9 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 22:31:35 2024
+
+@author: Henrick & Ivan (but really mostly Henrick)
+"""
+
 import csv
 import matplotlib.pyplot as plt
 import glob
 import os
 import sys
 import subprocess
+
+#Flags
+PLOT_PITCH = True
+PLOT_ROLL = False
+PLOT_YAW = False
+PLOT_AIRSPEED = False
+PLOT_ELEVATOR_SERVO_ACTUAL = True # this is just a rate plotted
+PLOT_ELEVATOR_ANGLE = True
+
+
+
 
 def openImage(path):
     imageViewerFromCommandLine = {'linux':'xdg-open',
@@ -51,32 +69,49 @@ def plot_the_csv_output():
     # plt.show()
     
     
+    #bad code but it works
+    column = []
     
-    WHICH_COLUMN_TO_PLOT = 2
+    if PLOT_PITCH:
+        column.append(["Pitch",2])
+    if PLOT_ROLL:
+        column.append(["Roll",3])
+    if PLOT_YAW:
+        column.append(["Yaw",4])
+    if PLOT_AIRSPEED:
+        column.append(["Airspeed",5])
+    if PLOT_ELEVATOR_SERVO_ACTUAL:
+        column.append(["Elevator Actual Rate",19])
+    if PLOT_ELEVATOR_ANGLE:
+        column.append(["Elevator Angle",20])
+        
+    for c in column:
+        
+        col_num = c[1]
+        
+        column1 = []
+        column2 = []
+        
+        with open(csv_file_path, 'r') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            for row in csv_reader:
+                # Assuming column 1 is at index 0 and column 2 is at index 1
+                column1.append(float(row[0]))
+                column2.append(float(row[col_num - 1]))
+        
+        plt.figure(figsize=(10, 3))
+        # Plotting column 2 vs column 1 as a red line graph with higher DPI
+        plt.plot(column1, column2, color='blue', linestyle='-', linewidth = 1.5)
+        plt.xlabel('Time Elapsed (s)')
+        plt.ylabel(c[0])
+        plt.title(c[0])
+        plt.grid(True)
+        image_save_path = os.path.join(root_path, 'temp_plots', c[0]+'.png')
+        plt.savefig(image_save_path, dpi=200)  # Save the figure with higher DPI
+        plt.show()
+        print("********* FILE SAVED AT: "+image_save_path + ' *********')
     
-    column1 = []
-    column2 = []
-    
-    with open(csv_file_path, 'r') as csvfile:
-        csv_reader = csv.reader(csvfile)
-        for row in csv_reader:
-            # Assuming column 1 is at index 0 and column 2 is at index 1
-            column1.append(float(row[0]))
-            column2.append(float(row[WHICH_COLUMN_TO_PLOT - 1]))
-    
-    plt.figure(figsize=(10, 3))
-    # Plotting column 2 vs column 1 as a red line graph with higher DPI
-    plt.plot(column1, column2, color='blue', linestyle='-', linewidth = 1.5)
-    plt.xlabel('Time Elapsed (s)')
-    plt.ylabel('Pitch (deg)')
-    plt.title('Pitch')
-    plt.grid(True)
-    image_save_path = os.path.join(root_path, 'temp_plots', 'Pitch.png')
-    plt.savefig(image_save_path, dpi=200)  # Save the figure with higher DPI
-    plt.show()
-    print("********* FILE SAVED AT: "+image_save_path + ' *********')
-
-    openImage(image_save_path)
+        #openImage(image_save_path)
 
     # WHICH_COLUMN_TO_PLOT = 3
     # 
@@ -100,4 +135,5 @@ def plot_the_csv_output():
     # plt.savefig('Roll.png', dpi=200)  # Save the figure with higher DPI
     # plt.show()
 
+plot_the_csv_output()
 
